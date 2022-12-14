47d5b528b75b7a654ff24da32c146bb5ed8b8873
Дима Ларионов <dima_larionov_000@mail.ru>
2022-12-15 00:20:04 +0300

    Добавьте файлы проекта.

---
+++ UniqueOrder/SearchForAUniqueOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UniqueOrder;
+
+public class SearchForAUniqueOrder
+{
+    public static List<char> UniqueInOrder(string enter)
+    {
+        List<char> result = new List<char>();
+        for (int i = 0; i < enter.Length; i++)
+        {
+            int a = i + 1;
+            if (a == enter.Length)
+            {
+                a = enter.Length - 1;
+            }
+            else
+            {
+                if (enter[i] != enter[i + 1])
+                {
+                    result.Add(enter[i]);
+                }
+            }
+        }
+        result.Add(enter[enter.Length - 1]);
+        return result;
+    }
+}
